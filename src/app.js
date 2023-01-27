bb6f0387ba9be0cd1469bf2b6cde46eac49e88fa
Julien Moreau <julien@thylo.be>
2023-01-27 18:18:49 +0100

    port

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ dotenv.config();
 mongoose.connect(process.env.DB_PATH);
 mongoose.set("strictQuery", false);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 //
 const app = express();
 app.use(cors());
